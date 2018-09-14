1168d047dbc9e60d202c839886d344ebec53a2b2
Max Ma <xuezhe.ma@gmail.com>
2018-09-13 21:07:14 -0400

    debug

--- mae/modules/encoders/gaussian_encoder.py
+++ mae/modules/encoders/gaussian_encoder.py
@@ -185,7 +185,12 @@ class GaussianEncoder(Encoder):
             # [batch * nsamples] --> [batch, nsamples]
             log_probs_prior = self.log_probability_prior(z.view(z_size[0] * z_size[1], *z_size[2:])).view(z_size[0], z_size[1])
             log_probs_posterior = self.log_probability_posterior(x, z, distr_params=distr_params)
+            print('prior')
             print(log_probs_prior)
+            input()
+            print('posterior')
+            print(log_probs_posterior)
+            input()
 
             # compute KL using MC [batch]
             KL = (log_probs_posterior - log_probs_prior).mean(dim=1)
